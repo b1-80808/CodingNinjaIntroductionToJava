bbf739b01b2b667b150b8d3fc3c9212051bd26bb
Sanket Vaidya <sanket.vaidya2023@gmail.com>
2023-07-14 22:11:44 +0530

    Final Decimal to Binary Program

--- MileStone2/src/Operators_and_for_loop/Decimal_To_Binary.java
+++ MileStone2/src/Operators_and_for_loop/Decimal_To_Binary.java
@@ -4,28 +4,19 @@ import java.util.Scanner;
 
 public class Decimal_To_Binary {
 
-	public static void main(String[] args) {
-
+	public static void main(String args[])
+	{
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Number in Decimal Form:- ");
+		System.out.print("Number in Decimal:- ");
 		int number = sc.nextInt();
-		int rem = 0;
-		int binary = 0;
-		int reverse =0;
+		long binary = 0,pow = 1;
 		while(number>0)
 		{
-			rem = number%2;
-			binary = rem * 10 + binary;
+			int rem = number%2;
+			binary+=rem*pow;
+			pow*=10;
 			number = number/2;
 		}
-		while(binary>0)
-		{
-			rem = binary%10;
-			reverse = reverse*10 + rem;
-			binary = binary/10;
-		}
-
-		System.out.print("Binary Form :- "+ reverse);
+		System.out.print("Binary:-"+ binary);
 	}
-
 }
