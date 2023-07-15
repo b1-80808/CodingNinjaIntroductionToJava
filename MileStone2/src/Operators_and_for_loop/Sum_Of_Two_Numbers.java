9a1717a47d8d171aace44eedbf64c88bdbb5daea
Sanket Vaidya <sanket.vaidya2023@gmail.com>
2023-07-15 17:07:54 +0530

    Exception Handling

---
+++ MileStone2/src/Operators_and_for_loop/Sum_Of_Two_Numbers.java
@@ -0,0 +1,22 @@
+package Operators_and_for_loop;
+import java.util.*;
+
+public class Sum_Of_Two_Numbers {
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int number1 =0,number2 =0;
+		try {
+			System.out.println("Enter first number : ");
+			 number1 = sc.nextInt();
+			System.out.println("Enter Second Number:- ");
+			number2 = sc.nextInt();
+		}catch(Exception e)
+		{
+			System.out.println("Enter Number in Integer Format Only!!!!!!!!");
+		}
+		
+		int result = number1 + number2;
+		System.out.println("Result " + result);
+	}
+}
