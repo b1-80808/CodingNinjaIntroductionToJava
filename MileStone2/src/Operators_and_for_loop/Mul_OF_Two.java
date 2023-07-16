bdbc1ec79435da7dc1975264df3a76ef7a4271e2
Sanket Vaidya <sanket.vaidya2023@gmail.com>
2023-07-16 20:51:25 +0530

    added division

--- MileStone2/src/Operators_and_for_loop/Mul_OF_Two.java
+++ MileStone2/src/Operators_and_for_loop/Mul_OF_Two.java
@@ -11,7 +11,9 @@ public class Mul_OF_Two {
 		System.out.println("Number2");
 		int number2 = sc.nextInt();
 		int result = number1*number2;
-		System.out.println("Result:- "+ result);
+		System.out.println("Multiplication:- "+ result);
+		int result1 = number1/number2;
+		System.out.println("Division:- " + result1);
 
 	}
 
