825d91788cd6c0e33a97cee7db3200fcb02216f8
Sanket Vaidya <sanket.vaidya2023@gmail.com>
2023-07-16 20:49:37 +0530

    Mul to Two

---
+++ MileStone2/src/Operators_and_for_loop/Mul_OF_Two.java
@@ -0,0 +1,18 @@
+package Operators_and_for_loop;
+
+import java.util.Scanner;
+
+public class Mul_OF_Two {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Number1");
+		int number1 = sc.nextInt();
+		System.out.println("Number2");
+		int number2 = sc.nextInt();
+		int result = number1*number2;
+		System.out.println("Result:- "+ result);
+
+	}
+
+}
