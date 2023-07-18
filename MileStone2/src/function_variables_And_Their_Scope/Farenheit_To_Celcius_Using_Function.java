6ac62e8efc087b2f09f61068423a2d17246865da
Sanket Vaidya <sanket.vaidya2023@gmail.com>
2023-07-18 16:31:25 +0530

    Farenheit to Celcius using functions

--- MileStone2/src/function_variables_And_Their_Scope/Farenheit_To_Celcius_Using_Function.java
+++ MileStone2/src/function_variables_And_Their_Scope/Farenheit_To_Celcius_Using_Function.java
@@ -1,5 +1,26 @@
 package function_variables_And_Their_Scope;
 
-public class Farenheit_To_Celcius_Using_Function {
+import java.util.Scanner;
 
+public class Farenheit_To_Celcius_Using_Function {
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Start Farenheit Value:- ");
+		int se = sc.nextInt();
+		System.out.println("End Farenheit Value:-  ");
+		int e = sc.nextInt();
+		System.out.println("Steps Between Values:- ");
+		int w = sc.nextInt();
+		printFarenheitTable(se,e,w);
+	}
+	
+	public static void printFarenheitTable(int startTemp,int EndTemp,int Gap)
+	{
+		for(;startTemp<=EndTemp;startTemp+=Gap)
+		{
+			int result =(int) ((5/9.0)*(startTemp - 32));
+			System.out.println("Fahrenheit: " + startTemp + "\tCelcius: " + result);
+		}
+	}
 }
